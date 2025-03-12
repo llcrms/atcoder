2b68d91080fd55b3c07d6a1d4172ef86dc5f639e
Koga Masato <koga.masato.0608@gmail.com>
2025-03-12 02:35:20 +0000

    complete D_Duplicated

--- past201912-open/D_Duplicated/Main.sh
+++ past201912-open/D_Duplicated/Main.sh
@@ -1,20 +1,25 @@
 read count;
-declare -a ary=($(seq 0 ${count}));
-ary[0]="";
-dup=""
+x=1
+before=0
+no_num=0
+dbl_num=0
 
-readarray -n ${count} -t lines
-
-for n in ${lines[*]}; do
-    if [[ ${ary[${n}]} = "" ]]; then
-        dup=${n}
-    else
-        ary[${n}]="";
+for n in `sort -n`; do
+    if [[ ${n} -eq ${before} ]]; then
+        dbl_num=${n}
+    elif [[ ${n} -eq $(($before + 2)) ]]; then
+        no_num=$(($before + 1))
     fi
+    if [[ ${no_num} -ne 0 ]] && [[ ${dbl_num} -ne 0 ]]; then
+        break;
+    fi
+    x=$(($x + 1))
+    before=${n}
 done
-if [[ ${dup} -eq 0 ]]; then
+if [[ ${no_num} -eq 0 ]] && [[ ${dbl_num} -eq 0 ]]; then
     echo Correct;
+elif [[ ${no_num} -eq 0 ]]; then
+    echo ${dbl_num} ${count}
 else
-    echo ${dup} ${ary[*]};
+    echo ${dbl_num} ${no_num}
 fi
-exit 0;
